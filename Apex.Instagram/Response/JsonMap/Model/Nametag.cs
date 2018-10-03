066235b58c856818e0afd8114f91fcd8607f3754
fagenorn <anisanissakkaf@gmail.com>
2018-10-03 16:31:16 +0200

    Fixed more jsonmaps types.

--- Apex.Instagram/Response/JsonMap/Model/Nametag.cs
+++ Apex.Instagram/Response/JsonMap/Model/Nametag.cs
@@ -1,5 +1,9 @@
 ﻿using System.Runtime.Serialization;
 
+using Apex.Instagram.Response.JsonFormatter;
+
+using Utf8Json;
+
 namespace Apex.Instagram.Response.JsonMap.Model
 {
     public class Nametag
@@ -8,12 +12,14 @@ namespace Apex.Instagram.Response.JsonMap.Model
         public int? Mode { get; set; }
 
         [DataMember(Name = "gradient")]
-        public string Gradient { get; set; }
+        [JsonFormatter(typeof(DurableUlongFormatter))]
+        public ulong? Gradient { get; set; }
 
         [DataMember(Name = "emoji")]
         public string Emoji { get; set; }
 
         [DataMember(Name = "selfie_sticker")]
-        public int? SelfieSticker { get; set; }
+        [JsonFormatter(typeof(DurableUlongFormatter))]
+        public ulong? SelfieSticker { get; set; }
     }
 }
